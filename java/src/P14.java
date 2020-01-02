b2d0b420d03997a77fc69e710f7d8b8d2e92720e
LenKIM <joenggyu0@gmail.com>
2020-01-02 20:22:33 +0900

    feats: week-5
    - 세금 계산기
    - 암호 검증
    - 합법적으로 운전 가능한 연령

---
+++ java/src/P14.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class P14 {
+
+    public static void main(String[] args) {
+        float WI_STATE = 0.55F;
+        Scanner sc = new Scanner(System.in);
+        float orderAmount = promptLongData("What is the order amount?", sc);
+        String state = promptStringData("What is the state?", sc);
+        float total = 0;
+        if ("WI".equals(state.trim())) {
+            total = WI_STATE + orderAmount;
+        }
+        System.out.println("The subtotal is $" + orderAmount);
+        System.out.println("The tax is " + WI_STATE);
+        System.out.println("The total is $" + total);
+    }
+
+    private static String promptStringData(String message, Scanner sc) {
+        System.out.println(message);
+        return sc.next();
+    }
+
+    private static long promptLongData(String message, Scanner sc) {
+        System.out.println(message);
+        while (!sc.hasNextLong()) {
+            sc.next();
+            System.out.println("Confirm your input");
+        }
+        return sc.nextLong();
+    }
+}
---
+++ java/src/P15.java
@@ -0,0 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * 암호 검증
+ * 암호는 미리 저장한 값과 사용자가 입력한 값을 비교하는 것으로 검증.
+ * 사용자 로그인 증명을 검증하는 간단한 프로그램 작성
+ */
+public class P15 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String hiddenPassword = "daou";
+
+        System.out.println("What is the password: ");
+        String password = sc.next();
+
+        if (Objects.equals(hiddenPassword.hashCode(), password.hashCode())) {
+            System.out.println("Welcome");
+        }
+    }
+}
