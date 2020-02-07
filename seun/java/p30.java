2150c35824448bfd844ac013d7fd7f179b8bec94
Lee Sang-Eun <seun0101@daou.co.kr>
2020-02-07 09:26:04 +0900

    불필요한 코드 제거

--- seun/java/p30.java
+++ seun/java/p30.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Created by SangEun on 2020-02-07.
@@ -11,9 +9,7 @@ import java.io.InputStreamReader;
  * 중첩된 루프를 사용할 것
  */
 public class p30 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) {
         for(int i = 0; i < 13; i++) {
             for(int j = 0; j < 13; j++) {
                 System.out.println(i + " x " + j  + " = " + i * j);
